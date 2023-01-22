ee26def63b34c8f6cbbd6bbb384b738d70d2db08
Виктория Князькова <vknyazkova01@gmail.com>
2023-01-22 14:46:47 +0300

    test commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import spacy
 #import ru_core_news_sm
 
 app = Flask(__name__)
-spacy.cli.download("ru")
+# spacy.cli.download("ru")
 nlp = spacy.load('ru_core_news_sm')
 
 
@@ -12,19 +12,18 @@ def main_page():
     return render_template('index.html')
 
 
-
 @app.route('/result', methods=['POST', 'GET'])
 def result():
     if request.method == "GET":
         return render_template("index.html")
 
     user_request = request.form['text']
+    print(user_request)
 
-    connection = sqlite3.connect('math_db_new_1.db')
-    cur = connection.cursor()
+    # connection = sqlite3.connect('math_db_new_1.db')
+    # cur = connection.cursor()
 
     return render_template('result.html')
-
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,15 +1,16 @@
 import re
+from collections import namedtuple
 
 
 def parse_request(user_request, spacy_lm):
     """
-
-    :param user_request:
-    :param spacy_lm:
-    :return:
+    Returns list of named tuples (text, lemma, pos)
     """
-    token_lemma_pos = [(t.text, t.lemma_, t.pos_)for t in spacy_lm(user_request)]
+
+    TokenInfo = namedtuple('TokenInfo', ['token', 'lemma', 'pos'])
+    token_lemma_pos = [TokenInfo(t.text, t.lemma_, t.pos_) for t in spacy_lm(user_request)]
     return token_lemma_pos
+
 
 def filter_selected_sentences(db_selected, user_request):
     """
@@ -17,7 +18,7 @@ def filter_selected_sentences(db_selected, user_request):
     :param db_selected: список из лемматизированных предложений из бд
     :param user_request: список из лемматизированных строк запроса
     """
-    
+
     reg_ex = '\s?([А-Яа-яёЁ]+)?\s?'.join(user_request)
     filtered = []
     for sent in db_selected:
