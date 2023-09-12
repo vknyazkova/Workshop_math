8e1153af7a983cc20fcfc9e1728d1cc7e3c5b80d
Виктория Князькова <vknyazkova01@gmail.com>
2023-09-13 00:42:21 +0300

    add new dataclasses
    Для способа заполнения html шаблона, где пробелы и знаки препинания будут под отдельным html тегом со своим форматированием.

--- website/backend/custom_dataclasses.py
+++ website/backend/custom_dataclasses.py
@@ -2,6 +2,33 @@ from dataclasses import dataclass
 from typing import Iterable
 
 
+# @dataclass
+# class HTMLSpan:
+#     text: str
+#     html_type: str = 'plain'
+#     color: str = 'black'
+#
+#
+# @dataclass
+# class HTMLQueryWord(HTMLSpan):
+#     html_type = 'tooltip'
+#     color = 'green'
+#     model: str = None
+#
+#
+# @dataclass
+# class HTMLResultWord(HTMLSpan):
+#     html_type: str = 'tooltip'
+#     pos: str = None
+#     lemma: str = None
+#
+#
+# @dataclass
+# class HTMLSentence:
+#     tokens: Iterable[HTMLSpan]
+#     youtube_link: str
+
+
 @dataclass
 class TokenInfo:
     token: str
@@ -40,7 +67,7 @@ class QueryInfo:
     "Датакласс со всей информацией о запросе"
 
     text: str
-    tokens: Iterable[QueryTokenInfo] = None
+    tokens: Iterable[QueryTokenInfo] = None  # переписать через фабрику типов
     pos_string: str = None
     lemmatized: str = None
     pictures: list = None
